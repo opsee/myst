a9ea374e6d30a18a8f62ba8ada7f78cd4b9dfd32
Sara Bee <sara.bainbridge@gmail.com>
2016-01-18 10:39:41 -0500

    Add restify server

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const logger = require('./utils/logger');
+const config  = require('config');
+const logger  = require('./utils/logger');
+const restify = require('restify');
 
-logger.info('hello world');
+const server = restify.createServer({
+  name: 'analytics'
+});
+
+server.get('/health', (req, res, next) => {
+  res.send(200);
+  next();
+});
+
+server.listen(config.server.port, () => {
+  logger.info(`${process.env.NODE_ENV} server ${server.name} listening at ${server.url}`);
+});
