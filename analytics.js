74518ab25990c89d4b959868641b47fbab55d6e8
Sara Bee <sara.bainbridge@gmail.com>
2016-01-19 11:55:01 -0500

    Split out intercom call into its own thing

--- analytics.js
+++ analytics.js
@@ -1,17 +1,35 @@
 const intercom = require('./lib/intercom');
+const Promise = require('bluebird');
+
+function formatEventString(category, action) {
+  return `${category} - ${action || ''}`;
+}
+
+function trackIntercom(category, action, user, data) {
+  const eventString = formatEventString(category, action);
+  const now = new Date();
+  const createdAt = Math.floor(now.getTime() / 1000);
+
+  return intercom.events.create({
+    event_name: eventString,
+    email: user.email,
+    created_at: createdAt,
+    update_last_request_at: true,
+    last_request_at: createdAt,
+    metadata: data
+  });
+}
 
 module.exports = {
 
   event(category, action, user, data) {
-    const now = new Date();
-    const createdAt = Math.floor(now.getTime() / 1000);
 
-    console.log(category, action, user, data);
+    return Promise.join(
+      trackIntercom(category, action, user, data),
 
-    return intercom.events.create({
-      event_name: 'test',
-      email: user.email,
-      created_at: createdAt
+      (intercomResponse) => {
+        console.log('intercom request:', intercomResponse.body);
+        return intercomResponse.body;
     });
   }
 };
