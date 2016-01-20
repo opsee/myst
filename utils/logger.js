2a015d17cd118dfb716843f62542ce5c690814a7
Sara Bee <sara.bainbridge@gmail.com>
2016-01-20 14:33:09 -0500

    Add timestamps to logs

--- utils/logger.js
+++ utils/logger.js
@@ -5,7 +5,9 @@ const winston = require('winston');
 const logger = new winston.Logger({
   level: 'info',
   transports: [
-    new (winston.transports.Console)(),
+    new (winston.transports.Console)({
+      timestamp: true
+    }),
   ]
 });
 
