fe053a99a80fe28503518aee958c8e7a5c737fe8
Sara Bee <sara.bainbridge@gmail.com>
2016-01-22 12:21:10 -0500

    Add tests for GET /health and POST /pageview

--- start-server.js
+++ start-server.js
@@ -1,7 +1,1 @@
-const config = require('config');
-const logger = require('./utils/logger');
-const server = require('./server');
-
-server.listen(config.server.port, () => {
-  logger.info(`${process.env.NODE_ENV} server ${server.name} listening at ${server.url}`);
-});
+require('./server').start();
---
+++ test/server-test.js
@@ -0,0 +1,88 @@
+const request = require('supertest');
+const server = require('../server');
+const assert = require('chai').assert;
+
+describe('server', () => {
+  before(() => {
+    server.start();
+  });
+
+  describe('POST /health', () => {
+    it('returns 200', (done) => {
+      request('http://localhost:9098')
+        .get('/health')
+        .expect(200, '', done);
+    });
+  });
+
+  describe('POST /pageview', () => {
+    it('returns 200', (done) => {
+      request('http://localhost:9098')
+        .post('/pageview')
+        .send({
+          path: '/test',
+          name: 'testing',
+          user: { id: 123 }
+        })
+        .expect(200, '', done);
+    });
+
+    it('returns error if path missing', (done) => {
+      request('http://localhost:9098')
+        .post('/pageview')
+        .send({
+          name: 'testing',
+          user: { id: 123 }
+        })
+        .expect(409, {
+          code: 'InvalidArgument',
+          message: 'Missing path parameter'
+        })
+        .end(done);
+    });
+
+    it('returns error if name missing', (done) => {
+      request('http://localhost:9098')
+        .post('/pageview')
+        .send({
+          path: '/test',
+          user: { id: 123 }
+        })
+        .expect(409, {
+          code: 'InvalidArgument',
+          message: 'Missing name parameter'
+        })
+        .end(done);
+    });
+
+    it('returns error if user missing', (done) => {
+      request('http://localhost:9098')
+        .post('/pageview')
+        .send({
+          path: '/test',
+          name: 'testing',
+        })
+        .expect(409, {
+          code: 'InvalidArgument',
+          message: 'Missing user.id parameter'
+        })
+        .end(done);
+    });
+
+    it('returns error if user.id missing', (done) => {
+      request('http://localhost:9098')
+        .post('/pageview')
+        .send({
+          path: '/test',
+          name: 'testing',
+          user: { email: 'foo@bar.com' }
+        })
+        .expect(409, {
+          code: 'InvalidArgument',
+          message: 'Missing user.id parameter'
+        })
+        .end(done);
+    });
+  });
+
+});
