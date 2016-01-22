c3c9f603354bb00bfbbebf715990dcc8436fe171
Sara Bee <sara.bainbridge@gmail.com>
2016-01-22 15:11:25 -0500

    Use ID for intercom.track

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ server.use(restify.bodyParser({ mapParams: true }));
  * @param {object} user - an object specifying one or more attributes for the
  *    user triggering the event.
  *
- * @param {String} user.email - (required) the email address for the user's account.
+ * @param {String} user.id - (required) the opsee ID of the user's account.
  *    This is used to identify them in intercom.io.
  *
  * @param {object} data - any additional JSON data to be tracked along with
